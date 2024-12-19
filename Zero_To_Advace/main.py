4e8707633b2abf81716eb0f86f5bf46dd35a9a84
Minhaz Uddin <uminhaz885@gmail.com>
2024-12-20 01:40:39 +0600

    Python basics

---
+++ Zero_To_Advace/main.py
@@ -0,0 +1,75 @@
+#create variables
+x=23
+y="Minhaz"
+print("The age of "+ y +"is "+ str(x))#because we can concatre only string
+
+#To get type of Variables
+print(type(y))
+#multiple variable
+a,b,c="Mango","Apple","Banana"
+print(a)
+print(b)
+print(c)
+
+#data types
+x1 = 20
+#display the data type of x:
+print(type(x1)) #int type
+
+x2 = 20.5
+#display the data type of x:
+print(type(x2))
+
+
+
+
+#Operator
+
+  # Operator: Arithmetic Operators
+
+# Initialize variables
+x1 = 10
+x2 = 5
+
+# Arithmetic operations
+print(x1 + x2)  # Addition
+print(x1 - x2)  # Subtraction
+print(x1 * x2)  # Multiplication
+print(x1 / x2)  # Division
+print(x1 % x2)  # Modulus
+print(x1 ** x2) # Exponentiation
+
+#comparison operator
+# Initialize variables
+x = 10
+y = 20
+
+# Comparison operators
+print(x == y)  # Equal to
+print(x != y)  # Not equal to
+print(x > y)   # Greater than
+print(x < y)   # Less than
+print(x >= y)  # Greater than or equal to
+print(x <= y)  # Less than or equal to
+
+if x>y:
+  print("x is greater than y")
+
+for i in range(5):
+    if i < 3:
+        print(f"{i} is less than 3")
+
+#logical operator and or not
+x=10
+y=6
+print((x>y)and (y<8))#this will pruint true or false ,here both condition should trtue
+
+print((x>y) or (y<8))#this will pruint true or false ,here at least one condition should true
+
+
+print(not(x > 5)) #false becaus x is greater than 5
+
+
+
+
+
